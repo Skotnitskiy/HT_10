0218182ac2dc9648955175c37ce1e56539910be2
serhSK <sergeyskotnitskiy@gmail.com>
2018-01-24 02:54:45 +0200

    added report template and jobstories view

--- ht10/ht10/settings.py
+++ ht10/ht10/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ht10.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
