b51d7d288915ce85c43e6e06b6911d2cd54e205d
serhSK <sergeyskotnitskiy@gmail.com>
2018-01-24 03:18:15 +0200

    show newstories

--- ht10/ht10/urls.py
+++ ht10/ht10/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from jobstories import views
+import jobstories.views
+import newstories.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobstories/', views.jobstories),
+    path('jobstories/', jobstories.views.jobstories),
+    path('newstories/', newstories.views.newstories),
 ]
